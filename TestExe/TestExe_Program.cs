394665561e7d99d9f2acfae7ea751d48440c3856
Kassiopeia <kasiananastasiia@gmail.com>
2025-03-18 17:07:21 +0000

    testing basic WebApi

---
+++ TestExe/TestExe_Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello from TestExe!");
+
+string uri = "http://localhost:5120";
+
+HttpClient httpClient = new HttpClient();
+HttpResponseMessage result = await httpClient.GetAsync(uri + "/weatherforecast");
+
+if (result.IsSuccessStatusCode)
+{
+    var stringData = await result.Content.ReadAsStringAsync();
+
+    Console.WriteLine(stringData);
+}
