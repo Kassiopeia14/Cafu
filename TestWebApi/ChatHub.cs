1bbd0507e436b10495b1ea1c80dc981ffb702578
Kassiopeia <kasiananastasiia@gmail.com>
2025-03-25 14:09:05 +0000

    saving messages to db

--- TestWebApi/ChatHub.cs
+++ TestWebApi/ChatHub.cs
@@ -1,9 +1,23 @@
 using Microsoft.AspNetCore.SignalR;
+using modTestChatRepository;
+using modTestWebApiJSONModels;
 
 public class ChatHub : Hub
 {
+    ITestChatRepository chatRepository;
+
+    public ChatHub(ITestChatRepository chatRepository)
+    {
+        this.chatRepository = chatRepository;
+    }       
+
     public async Task SendMessage(string user, string message)
     {
+        chatRepository.SaveMessage(
+            user, 
+            $"everyone_except_{user}", 
+            new MessageItem { Text = message });
+
         await Clients
             .AllExcept(Context.ConnectionId)
             .SendAsync("ReceiveMessage", user, message);
